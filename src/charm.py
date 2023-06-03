3175828f2b0736aca6b666c5d8e91a989eb2f0cb
Jose C. Massón <jose.masson@canonical.com>
2023-06-03 02:19:32 -0300

    Snips - Traefik integration

--- src/charm.py
+++ src/charm.py
@@ -14,48 +14,121 @@ https://discourse.charmhub.io/t/4208
 
 import logging
 import secrets
+import socket
 import string
+from urllib.parse import urlparse
 
-import ops
+from charms.traefik_k8s.v1.ingress import IngressPerAppRequirer
+from ops import PebbleReadyEvent
+from ops.charm import CharmBase
 from ops.framework import StoredState
+from ops.main import main
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    MaintenanceStatus,
+)
+from ops.pebble import ChangeError, Layer
 
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
 
-class SnipsK8SOperatorCharm(ops.CharmBase):
+class SnipsK8SOperatorCharm(CharmBase):
     """Charm the service."""
 
     _log_path: str = "/var/log/snips.log"
-    _port: int = 80
+    _http_port: int = 8080
+    _ssh_port: int = 2222
+    _container_name = _layer_name = _service_name = "snips"
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
         self._stored.set_default(hmac_key="")
+        self.container = self.unit.get_container(self._container_name)
+        self.ingress = IngressPerAppRequirer(self, port=self._http_port, strip_prefix=True)
         self.framework.observe(self.on.snips_pebble_ready, self._on_snips_pebble_ready)
+        self.framework.observe(self.ingress.on.ready, self._handle_ingress)
+        self.framework.observe(self.ingress.on.revoked, self._handle_ingress)
 
-    def _on_snips_pebble_ready(self, event: ops.PebbleReadyEvent):
-        container = event.workload
-        container.add_layer("snips", self._pebble_layer, combine=True)
-        container.replan()
-        self.unit.status = ops.ActiveStatus()
+    def _on_snips_pebble_ready(self, event: PebbleReadyEvent):
+        self._common_exit_hook()
+
+    def _handle_ingress(self, _):
+        if url := self.ingress.url:
+            logger.info("Ingress is ready: '%s'.", url)
+        else:
+            logger.info("Ingress revoked.")
+        self._common_exit_hook()
+
+    def _common_exit_hook(self) -> None:
+        """Event processing hook that is common to all events to ensure idempotency."""
+        if not self.container.can_connect():
+            self.unit.status = MaintenanceStatus("Waiting for pod startup to complete")
+            return
+
+        # Make sure the external url is valid
+        if external_url := self._external_url:
+            parsed = urlparse(external_url)
+            if not (parsed.scheme in ["http", "https"] and parsed.hostname):
+                # This shouldn't happen
+                logger.error(
+                    "Invalid external url: '%s'; must include scheme and hostname.",
+                    external_url,
+                )
+                self.unit.status = BlockedStatus(
+                    f"Invalid external url: '{external_url}'; must include scheme and hostname."
+                )
+                return
+
+        # Update pebble layer
+        self._update_layer()
+        self.unit.status = ActiveStatus()
+
+    def _update_layer(self) -> bool:
+        """Update service layer.
+
+        Returns:
+          True if anything changed; False otherwise
+        """
+        overlay = self._pebble_layer
+        plan = self.container.get_plan()
+
+        if self._service_name not in plan.services or overlay.services != plan.services:
+            self.container.add_layer(self._layer_name, overlay, combine=True)
+            try:
+                self.container.replan()
+                return True
+            except ChangeError as e:
+                logger.error(
+                    "Failed to replan; pebble plan: %s; %s",
+                    self.container.get_plan().to_dict(),
+                    str(e),
+                )
+                return False
+
+        return False
 
     @property
-    def _pebble_layer(self):
-        return {
-            "summary": "snips layer",
-            "description": "pebble config layer for snips",
-            "services": {
-                "snips": {
-                    "override": "replace",
-                    "summary": "snips",
-                    "command": '/bin/sh -c "/usr/bin/snips.sh | tee {}"'.format(self._log_path),
-                    "startup": "enabled",
-                    "environment": self._env_vars,
-                }
-            },
-        }
+    def _pebble_layer(self) -> Layer:
+        return Layer(
+            {
+                "summary": "snips layer",
+                "description": "pebble config layer for snips",
+                "services": {
+                    "snips": {
+                        "override": "replace",
+                        "summary": "snips",
+                        "command": '/bin/sh -c "/usr/bin/snips.sh | tee {}"'.format(
+                            self._log_path
+                        ),
+                        "startup": "enabled",
+                        "environment": self._env_vars,
+                    }
+                },
+            }
+        )
 
     @property
     def _env_vars(self) -> dict:
@@ -63,6 +136,9 @@ class SnipsK8SOperatorCharm(ops.CharmBase):
             "SNIPS_DEBUG": True,
             "SNIPS_HMACKEY": self._hmac_key,
         }
+
+        if self.ingress.url:
+            env_vars["SNIPS_HTTP_EXTERNAL"] = self.ingress.url
 
         return env_vars
 
@@ -78,6 +154,16 @@ class SnipsK8SOperatorCharm(ops.CharmBase):
         chars = string.ascii_letters + string.digits
         return "".join(secrets.choice(chars) for _ in range(24))
 
+    @property
+    def _internal_url(self) -> str:
+        """Return the fqdn dns-based in-cluster (private) address."""
+        return f"http://{socket.getfqdn()}:{self._http_port}"
+
+    @property
+    def _external_url(self) -> str:
+        """Return the externally-reachable (public) address."""
+        return self.ingress.url or self._internal_url
+
 
 if __name__ == "__main__":  # pragma: nocover
-    ops.main(SnipsK8SOperatorCharm, use_juju_for_storage=True)
+    main(SnipsK8SOperatorCharm, use_juju_for_storage=True)
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -4,6 +4,7 @@
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
 import unittest
+from unittest.mock import PropertyMock, patch
 
 import ops
 import ops.testing
@@ -12,57 +13,41 @@ from charm import SnipsK8SOperatorCharm
 
 class TestCharm(unittest.TestCase):
     def setUp(self):
+        mock_hmac_key = patch(
+            "charm.SnipsK8SOperatorCharm._hmac_key", new_callable=PropertyMock, return_value="D10S"
+        )
+        self.mock_hmac_key = mock_hmac_key.start()
         self.harness = ops.testing.Harness(SnipsK8SOperatorCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_httpbin_pebble_ready(self):
+    def test_snips_pebble_ready(self):
         # Expected plan after Pebble ready with default config
         expected_plan = {
             "services": {
-                "httpbin": {
+                "snips": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "snips",
+                    "command": '/bin/sh -c "/usr/bin/snips.sh | tee /var/log/snips.log"',
                     "startup": "enabled",
-                    "environment": {"GUNICORN_CMD_ARGS": "--log-level info"},
+                    "environment": {
+                        "SNIPS_DEBUG": True,
+                        "SNIPS_HMACKEY": "D10S",
+                    },
                 }
-            },
+            }
         }
         # Simulate the container coming up and emission of pebble-ready event
-        self.harness.container_pebble_ready("httpbin")
+        self.harness.container_pebble_ready("snips")
         # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("httpbin").to_dict()
+
+        # import pdb
+        # pdb.set_trace()
+        updated_plan = self.harness.get_container_pebble_plan("snips").to_dict()
         # Check we've got the plan we expected
         self.assertEqual(expected_plan, updated_plan)
         # Check the service was started
-        service = self.harness.model.unit.get_container("httpbin").get_service("httpbin")
+        service = self.harness.model.unit.get_container("snips").get_service("snips")
         self.assertTrue(service.is_running())
         # Ensure we set an ActiveStatus with no message
         self.assertEqual(self.harness.model.unit.status, ops.ActiveStatus())
-
-    def test_config_changed_valid_can_connect(self):
-        # Ensure the simulated Pebble API is reachable
-        self.harness.set_can_connect("httpbin", True)
-        # Trigger a config-changed event with an updated value
-        self.harness.update_config({"log-level": "debug"})
-        # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("httpbin").to_dict()
-        updated_env = updated_plan["services"]["httpbin"]["environment"]
-        # Check the config change was effective
-        self.assertEqual(updated_env, {"GUNICORN_CMD_ARGS": "--log-level debug"})
-        self.assertEqual(self.harness.model.unit.status, ops.ActiveStatus())
-
-    def test_config_changed_valid_cannot_connect(self):
-        # Trigger a config-changed event with an updated value
-        self.harness.update_config({"log-level": "debug"})
-        # Check the charm is in WaitingStatus
-        self.assertIsInstance(self.harness.model.unit.status, ops.WaitingStatus)
-
-    def test_config_changed_invalid(self):
-        # Ensure the simulated Pebble API is reachable
-        self.harness.set_can_connect("httpbin", True)
-        # Trigger a config-changed event with an updated value
-        self.harness.update_config({"log-level": "foobar"})
-        # Check the charm is in BlockedStatus
-        self.assertIsInstance(self.harness.model.unit.status, ops.BlockedStatus)
